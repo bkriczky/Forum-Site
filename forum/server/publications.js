4ea6fa404224cc4bc5d4b18bcc73218f2555e779
Brian <brian.kriczky@gmail.com>
2017-06-01 09:37:42 -0400

    Initial Commit

---
+++ forum/server/publications.js
@@ -0,0 +1,20 @@
+
+Meteor.publish('topic', function(id){
+  check(id, String);
+  return Topics.find({_id: id});
+});
+
+Meteor.publish('topics', function(){
+  return Topics.find();
+});
+
+
+Meteor.publish('threads', function(topicId){
+  check(topicId, String);
+  return Threads.find({topicId: topicId});
+});
+
+Meteor.publish('thread', function(id){
+  check(id, String);
+  return Threads.find({_id: id});
+});
